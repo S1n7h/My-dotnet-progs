185712fe864ac5a9f8091569b5a484fbdf087bc1
Soumyadeep Mullick <soumyadeepmullick7@gmail.com>
2026-03-30 01:00:17 +0530

    figuring stuff out with the front end

--- GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af01ada25b47ed53d8cef2756ca8a35902a2acb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ GameStore.Frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+const BASE_URL = "http://localhost:5073";
+
+function App() {
+    const [id, setId] = useState("");
+
+    
+    return (
+        <div>
+        <h1>Game Finder</h1>
+        </div>
+        <input value={id} 
+    );
+}
+
+// export default App
