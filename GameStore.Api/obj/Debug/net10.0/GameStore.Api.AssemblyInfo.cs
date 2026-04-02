be4b4e27530fbbce883a1b2fa1a6653801d7b686
Soumyadeep Mullick <soumyadeepmullick7@gmail.com>
2026-04-03 02:01:22 +0530

    les goo

--- GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+340c41a775fb787530c8c5201298442559e36204")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a94101748311bb02c44b89d21494fb977b7752d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
