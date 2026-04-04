b836d54c1c8c49fc7416521dcc0f8047c2defc3e
Soumyadeep Mullick <soumyadeepmullick7@gmail.com>
2026-04-05 00:37:20 +0530

    added options to delete, get and print games separately still need to work on implementing resetting different variables after await calls

--- GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a94101748311bb02c44b89d21494fb977b7752d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be4b4e27530fbbce883a1b2fa1a6653801d7b686")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- GameStore.Frontend/src/App.tsx
+++ GameStore.Frontend/src/App.tsx
@@ -4,57 +4,110 @@ import './App.css'
 const BASE_URL = "http://localhost:5073";
 
 function App() {
+    const [mode, setMode] = useState<string>("");
     const [id, setId] = useState("");
     const [game, setGame] = useState<any>(null);
     const [games, setGames] = useState<any>([]);
+    const [price, setPrice] = useState("");
+    const [name, setName] = useState("");
+    const [genreId, setGenreId] = useState("");
+    const [releaseDate, setReleaseDate] = useState("");
        
     const getGameUsingID = async () => {
         const Game = await fetch(`${BASE_URL}/games/${id}`);
         const data = await Game.json();
         console.log(data);
-        setGame(data);
+        await setGame(data);
     };
 
     const getGames = async () => {
         const Games = await fetch(`${BASE_URL}/games`);
         const data = await Games.json();
         console.log(data);
-        setGames(data);
-    }
+        await setGames(data);
+    };
+
+    const addGame = async (name: string, price: string, genreId: string, releaseDate: string) => {
+        const Game = await fetch(`${BASE_URL}/games`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                name: name,
+                genreId: Number(genreId),
+                price: Number(price),
+                releaseDate: releaseDate
+            })
+        });
+        
+        console.log("STATUS:", Game.status);
+        const data = await Game.text();
+        console.log("RESPONSE:", data);
+        await getGames();
+        console.log("Added game");
+    };
+
+    const deleteGame = async (id: number) => {
+        const Game = await fetch (`${BASE_URL}/games/${id}`, {            
+            method: "DELETE"
+        });
+
+        //after deletion, you want the UI refreshes
+        await getGames();
+    };
 
     return (
         <div className="container">
-            
             <h1>🎮 Game Finder</h1>
+            <button onClick={() => setMode("get")}>Get</button>
+            <button onClick={() => setMode("add")}>Add</button><button onClick={async () => {
+                setMode("list"); // set the mode to show the list
+                await getGames(); // fetch all games
+            }}>
+            Show All</button>
 
-            <div className="search-section">
-            <input
-                onChange={(e) => setId(e.target.value)}
-                placeholder="Enter ID"
-            />
-            <button onClick={getGameUsingID}>Get Game</button>
-            </div>
+            {mode == "get" && (
+                <>
+                    <div className="search-section">
+                        <input onChange={(e) => setId(e.target.value)} placeholder="Enter ID" />
+                        <button onClick={getGameUsingID}>Get Game</button>
+                    </div>
 
-            {game && (
-            <div className="card">
-                <h2>{game.name}</h2>
-                <p>₹ {game.price}</p>
-            </div>
+                    {game && (
+                    <div className="card">
+                        <h2>{game.name}</h2>
+                        <p>₹ {game.price}</p>
+                    </div>
+                    )}
+                </>
             )}
 
-            <div className="all-games">
-            <button onClick={getGames}>Get All Games</button>
+            {mode == "add" && (
+                <>
+                    <input placeholder="Name" onChange={(e) => setName(e.target.value)} />
+                    <input placeholder="Price" onChange={(e) => setPrice(e.target.value)} />
+                    <input placeholder="Genre ID" onChange={(e) => setGenreId(e.target.value)} />
+                    <input placeholder="Release Date" onChange={(e) => setReleaseDate(e.target.value)} />
+                    <button onClick={() => addGame(name, price, genreId, releaseDate)}>Add Game</button>
+                </>                
+            )}
 
-            <div className="games-list">
-                {games.map((g: any) => (
-                <div className="card" key={g.id}>
-                    <h3>{g.name}</h3>
-                    <p>₹ {g.price}</p>
+            {mode == "list" && (
+                <div className="all-games">
+
+                <div className="games-list">
+                    {games.map((g: any) => (
+                    <div className="card" key={g.id}>
+                        <h3>{g.name}</h3>
+                        <p>₹ {g.price}</p>
+
+                        <button onClick={() => deleteGame(g.id)}>Delete</button>
+                    </div>
+                    ))}
                 </div>
-                ))}
-            </div>
-            </div>
-
+                </div>
+            )}
         </div>
         );
 }
